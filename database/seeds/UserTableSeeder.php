29cc46fded98fb1efc34d6734a1383e146a15bb9
Israel Martínez Carmona <Sledgehammer94@MacBook-Air-de-Israel.local>
2016-11-19 23:08:13 -0600

    Se suben las semillas para pruebas de consulta

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +12,49 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+    	DB::table('user')->insert([
+            'name' => 'Cesar',
+            'nickName' => 'Cesar',
+            'lastName' => 'Garcia',
+            'email' => 'cesargc0507@gmail.com',
+            'password' => bcrypt('secret'),
+            'birthName' => '1994-07-05'
+        ]);
+
+        DB::table('user')->insert([
+            'name' => 'Israel',
+            'nickName' => 'Israel',
+            'lastName' => 'Martinez',
+            'email' => 'israel@gmail.com',
+            'password' => bcrypt('secret'),
+            'birthName' => '1994-09-02'
+        ]);
+
+        DB::table('user')->insert([
+            'name' => 'Ulises',
+            'nickName' => 'Ulises',
+            'lastName' => 'Garcia',
+            'email' => 'ulises@gmail.com',
+            'password' => bcrypt('secret'),
+            'birthName' => '1998-05-12'
+        ]);
+
+        DB::table('user')->insert([
+            'name' => 'Eduardo',
+            'nickName' => 'Eduardo',
+            'lastName' => 'Rodriguez',
+            'email' => 'eduardo@gmail.com',
+            'password' => bcrypt('secret'),
+            'birthName' => '1993-11-18'
+        ]);
+
+        DB::table('user')->insert([
+            'name' => 'Aldo',
+            'nickName' => 'Aldo',
+            'lastName' => 'Gutierrez',
+            'email' => 'aldo@gmail.com',
+            'password' => bcrypt('secret'),
+            'birthName' => '1990-08-12'
+        ]);
     }
 }
